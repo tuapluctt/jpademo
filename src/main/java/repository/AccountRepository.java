580c80df7147bbe8db430b8d8f848862cc8e5af8
LoanNguyenThi5 <Loan.NguyenThi5@nashtechglobal.com>
2023-05-12 20:48:31 +0700

    commit jpa

--- src/main/java/repository/AccountRepository.java
+++ src/main/java/repository/AccountRepository.java
@@ -2,9 +2,17 @@ package repository;
 
 import entity.AccountEntity;
 import entity.CategoryEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface AccountRepository extends CrudRepository<AccountEntity, Integer> {
+    @Query(value = "",nativeQuery = true)
+    void updateAccLook();
+
+    @Query("delete account u where u.balance = 0")
+    void deleteAccByBalanc();
 }
